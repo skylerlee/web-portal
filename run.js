305fd2d1a38acdacfb73c11ba39d72765208e65d
skylerlee <skyler.ac.lee@gmail.com>
2022-02-18 01:41:55 +0800

    feat: update run

--- run.js
+++ run.js
@@ -1,3 +1,27 @@
-const app = require('./lib');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const config = require('config');
+const lib = require('./lib');
 
-app.init().listen(3000);
+function mapFiles(conf) {
+  const keys = Object.keys(conf);
+  return keys.reduce((prev, key) => {
+    const val = prev[key];
+    if (typeof val === 'string') {
+      prev[key] = fs.readFileSync(val);
+    }
+    return prev;
+  }, {});
+}
+
+function run() {
+  const local = config.get('local');
+  const app = lib.init();
+  const server = local.ssl
+    ? https.createServer(mapFiles(local.ssl), app.callback())
+    : http.createServer(app.callback());
+  server.listen(local.port);
+}
+
+run();
