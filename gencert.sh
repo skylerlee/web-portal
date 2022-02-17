09b1aac53cbed8b2834bd1d1769713965281872c
skylerlee <skyler.ac.lee@gmail.com>
2022-02-18 01:54:22 +0800

    feat: add cert

---
+++ gencert.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+pushd ./ssl
+openssl genrsa -out key.pem
+openssl req -new -key key.pem -out csr.pem
+openssl x509 -req -days 9999 -in csr.pem -signkey key.pem -out cert.pem
+rm csr.pem
+popd
